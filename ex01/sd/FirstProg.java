b01deccd386047d50177eadaa032353e1b25d749
carlessAngeleno <daydream.han@gmail.com>
2015-04-05 15:51:32 -0700

    Use format where you keep study drill files in a subfolder; complete ex02

--- ex01/sd/FirstProg.java
+++ ex01/sd/FirstProg.java
@@ -1,4 +1,4 @@
-public class FirstProgSd
+public class FirstProg
 {
     public static void main( String[] args )
     {
---
+++ ex02/sd/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "+---------------------------------------------------------+" );
+        System.out.println( "|                                                    #### |" );
+        System.out.println( "|                                                    #### |" );
+        System.out.println( "|                                                    #### |" );
+        System.out.println( "|                                                         |" );
+        System.out.println( "|                                                         |" );
+        System.out.println( "|                              Han Kim                    |" );
+        System.out.println( "|                              My Address                 |" );
+        System.out.println( "|                              City, State ZIP            |" );
+        System.out.println( "|                                                         |" );
+        System.out.println( "+---------------------------------------------------------+" );
+    }
+}
