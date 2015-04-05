e9af326e2786373602b38f80d71b33ba6113912a
carlessAngeleno <daydream.han@gmail.com>
2015-04-05 15:37:01 -0700

    Complete Study Drill for ex01

---
+++ ex01/FirstProgSd.java
@@ -0,0 +1,8 @@
+public class FirstProgSd
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I, Han Kim, am determined to learn how to code." );
+        System.out.println( "Today's date is April 04, 2015." );    
+    }
+}
