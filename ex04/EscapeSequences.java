9ab84655f138a2f597d77c1f7ada35803df9549e
carlessAngeleno <daydream.han@gmail.com>
2015-04-06 22:16:09 -0700

    Finish ex04

--- ex04/EscapeSequences.java
+++ ex04/EscapeSequences.java
@@ -2,7 +2,15 @@ public class EscapeSequences
 {
     public static void main( String[] args )
     {
-        // Initial version created using FIGlet, font "Big Money", oriented
-        // southwest
+        // Initial version created using FIGlet, font "Big Money", oriented southwest
 
-        System.out.print( "\t    
+        System.out.print( "\t    _____\n\t   /     |\n\t   JJJJJ |" );
+        System.out.println( "  ______   __     __  ______" );
+        System.out.println( "\t      JJ | /      \\ /  \\   /  |/      \\" );
+        System.out.println( "\t __   JJ | aaaaaa  |\"\"  \\ /\"\"/ aaaaaa  |" );
+        System.out.println( "\t/  |  JJ | /    aa | \"\"  /\"\"/  /    aa |" );
+        System.out.println( "\tJJ \\__JJ |/aaaaaaa |  \"\" \"\"/  /aaaaaaa |" );
+        System.out.println( "\tJJ    JJ/ aa    aa |   \"\"\"/   aa    aa |" );
+        System.out.println( "\t JJJJJJ/   aaaaaaa/     \"/     aaaaaaa/" );
+    }
+}
